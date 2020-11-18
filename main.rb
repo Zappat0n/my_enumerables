9e0c43cfe3b9ae180b895d2b62394293ef406741
Angel Barros <albarros1973@gmail.com>
2020-11-18 11:57:58 +0100

    Added my_map_proc

--- main.rb
+++ main.rb
@@ -99,9 +99,22 @@ module Enumerable
     total
   end
 
-
   # Modify your #my_map method to take a proc instead.
-  # Modify your #my_map method to take either a proc or a block. It won't be necessary to apply both a proc and a block in the same #my_map call since you could get the same effect by chaining together one #my_map call with the block and one with the proc. This approach is also clearer, since the user doesn't have to remember whether the proc or block will be run first. So if both a proc and a block are given, only execute the proc.
+  def my_map_proc(proc)
+    arr = []
+    my_each do |value|
+      arr.push(proc.call(value))
+    end
+    arr
+  end
+
+  # Modify your #my_map method to take either a proc or a block.
+  # It won't be necessary to apply both a proc and a block in the same #my_map call
+  # since you could get the same effect by chaining together one #my_map call with the block
+  # and one with the proc. This approach is also clearer, since the user doesn't have to remember
+  # whether the proc or block will be run first.
+  # So if both a proc and a block are given, only execute the proc.
+  
 end
 
 # Test your #my_inject by creating a method called #multiply_els which multiplies all 
@@ -110,9 +123,8 @@ def multiply_els(arr)
   arr.my_inject(1) { |total, x| total * x }
 end
 
+my_proc = proc { |x| x + 1 }
+p (1..9).my_map_proc(my_proc)
 
-#p (1..9).my_inject(5) { |total, x| total + x }
-
-p multiply_els([2, 4, 5])
 
 # p [2, 4].my_none? { |x| x % 2 == 0 }
