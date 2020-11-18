ed8f0944d6c1d4641154d31c532657e039247000
Angel Barros <albarros1973@gmail.com>
2020-11-18 11:29:44 +0100

    Added my_inject

--- main.rb
+++ main.rb
@@ -7,6 +7,8 @@ module Enumerable
   # You'll need to remember the yield statement. 
   # Make sure it returns the same thing as #each as well.
   def my_each
+    return 'No block given' unless block_given?
+
     for value in self do
       yield(value)
     end
@@ -14,6 +16,8 @@ module Enumerable
 
   # Create #my_each_with_index in the same way.
   def my_each_with_index
+    return 'No block given' unless block_given?
+
     index = 0
     for value in self do
       yield(value, index)
@@ -23,6 +27,8 @@ module Enumerable
 
   # Create #my_select in the same way, though you may use #my_each in your definition (but not #each).
   def my_select
+    return 'No block given' unless block_given?
+
     arr = []
     my_each do |value|
       arr.push(value) if yield(value)
@@ -31,6 +37,8 @@ module Enumerable
 
   # Create #my_all? (continue as above)
   def my_all?
+    return 'No block given' unless block_given?
+
     result = true
     my_each do |value|
       return false unless yield(value)
@@ -40,6 +48,8 @@ module Enumerable
 
   # Create #my_any?
   def my_any?
+    return 'No block given' unless block_given?
+
     result = false
     my_each do |value|
       return true if yield(value)
@@ -49,6 +59,8 @@ module Enumerable
 
   # Create #my_none?
   def my_none?
+    return 'No block given' unless block_given?
+
     result = true
     my_each do |value|
       return false if yield(value)
@@ -67,6 +79,8 @@ module Enumerable
 
   # Create #my_map
   def my_map
+    return 'No block given' unless block_given?
+
     arr = []
     my_each do |value|
       arr.push(yield(value))
@@ -75,11 +89,21 @@ module Enumerable
   end
 
   # Create #my_inject
+  def my_inject(initial = 0)
+    return 'No block given' unless block_given?
+
+    total = initial
+    my_each do |x|
+      total = yield(total, x)
+    end
+    total
+  end
+
   # Test your #my_inject by creating a method called #multiply_els which multiplies all the elements of the array together by using #my_inject, e.g. multiply_els([2,4,5]) #=> 40
   # Modify your #my_map method to take a proc instead.
   # Modify your #my_map method to take either a proc or a block. It won't be necessary to apply both a proc and a block in the same #my_map call since you could get the same effect by chaining together one #my_map call with the block and one with the proc. This approach is also clearer, since the user doesn't have to remember whether the proc or block will be run first. So if both a proc and a block are given, only execute the proc.
 end
 
-p (1..9).my_map { |x| x * 2 }
+p (1..9).my_inject(5) { |total, x| total + x }
 
 # p [2, 4].my_none? { |x| x % 2 == 0 }
